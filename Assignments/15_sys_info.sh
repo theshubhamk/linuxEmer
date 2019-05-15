3da4f456f958fc350a26c640d033d11d9cca9b86
SHUBHAM <shubhamkumar50@yahoo.com>
2019-05-15 12:33:44 +0530

    15/5

--- Assignments/15_sys_info.sh
+++ Assignments/15_sys_info.sh
@@ -17,7 +17,7 @@
 
 
 
-echo enter the number as per req. Write a script to print the following:
+echo enter the number as per req.
 echo 1. Currently logged users
 echo 2. Your shell directory
 echo 3. Home directory
@@ -31,45 +31,60 @@ echo 10.Memory information
 echo 11.File system information
 echo 12.Currently running process
 
+
+echo ENTER THE INDEX NO. OF THE REQD INFO
 read x
 
 case $x in
 	1)
+		echo Currently logged users
 		who
 		;;
-	2)
+	2) 
+		echo Your shell directory
 		pwd
 		;;
 	3)
+		
+		echo Home directory
 		cd
 		pwd
 		;;
-	4)
+	4) 
+		echo OS name and version
 		lsb_release -a
 		;;
-	5)
+	5)	
+		echo Current working directory
 		pwd
 		;;
-	6)
+	6) 
+		echo Number of users logged in
 		who -u
 		;;
 	7)
+		echo Show all available shells in your system
 		cd
 		cat /etc/shells
 		;;
 	8)	
+		echo Hard disk information
 		sudo parted -l
 		;;
-	9)
+	9)	
+		echo CPU information
 		lscpu
 		;;
 	10)	
+		echo Memory information
 		df -h
 		;;
-	11)
+	11)	 
+		echo File system information
 		df -Th
 		;;
 	12)
+		echo Currently running process
 		top
 		;;
 esac
