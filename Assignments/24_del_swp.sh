564b04e79cf9e5434243f6e795a7dbee5b2ad13b
SHUBHAM <shubhamkumar50@yahoo.com>
2019-05-19 20:38:37 +0530

    19/5

--- Assignments/24_del_swp.sh
+++ Assignments/24_del_swp.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-#Written by SHUBHAM KUMAR SINGH
-#script to delete all the .swp files found in your system or directory.
 
-if [ -n $1 ]
-then
-	cd $1
-	find . -name *.swp -delete
-else
-	find . -name *.swp -delete
-fi
+shopt -s dotglob
+rm *swp
