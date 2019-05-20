0a5977de0135bd4acc45e9638278cce3e709254a
SHUBHAM <shubhamkumar50@yahoo.com>
2019-05-20 14:56:00 +0530

    20/5

--- Assignments/17_rename_file_dir.sh
+++ Assignments/17_rename_file_dir.sh
@@ -1,6 +1,25 @@
 #!/bin/bash
-#Written by SHUBHAM KUMAR SINGH
-#script to rename a file/directory replaced by lower/upper case letters.
+
+arr1=(`find -type d | sed 1d | cut -d/ -f2`)
+arr2=(`find -type d | sed 1d | cut -d/ -f2 | tr [:lower:] [:upper:]`)
+
+arr3=(`find -maxdepth 1 -type f | sed 1d | cut -d/ -f2`)
+arr4=(`find -maxdepth 1 -type f | sed 1d | cut -d/ -f2 | tr [:upper:] [:lower:]`)
+y=-1
+z=-1
+for x in ${arr1[*]}
+do
+	let y=$y+1
+	mv $x ${arr2[$y]}
+done
+
+for p in ${arr3[*]}
+do
+	let z=$z+1
+	mv $p ${arr4[$z]}
+done
 
 
-arr=$(find . -name "*" -type d | tr [a-z] [A-Z])
+
+
+
