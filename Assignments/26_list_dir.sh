6a6eec5afc520db0092b43dea3800e2fa4fe5f5f
SHUBHAM <shubhamkumar50@yahoo.com>
2019-05-22 14:50:28 +0530

    22/5

--- Assignments/26_list_dir.sh
+++ Assignments/26_list_dir.sh
@@ -3,28 +3,51 @@
 #Script that takes any no. of directories as CL argument and then lists their content.
 
 
+
+
 p=$#
-count=0
-arr=(`find . -type d | sed 1d`)
+arr=($@)
+
 if [ $p -eq 0 ]
 then
-	for y in ${arr[*]}
-	do	
-		echo In directory - $y :
-		cd $y
-		find *
-		cd ..
+	for i in $(echo *)
+	do
+		echo $i
 	done
 else
-	for x in $*
+	for x in ${arr[*]}
 	do
-		if [ $count -le $# ]
-		then
-			cd $x
-			echo In directory --  $x :
-			find *
-			cd ..
-			let count=$count+1
-		fi
+		echo $x ':'
+		cd $x
+		for i in $(echo *)
+	  	do
+ 	               echo $i
+	        done
+		cd ..
+		echo -e -n "\n"
 	done
-fi	
+fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
