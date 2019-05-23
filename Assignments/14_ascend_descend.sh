a1174529dfe9ee23a9278ccdee00047645c7480a
SHUBHAM <shubhamkumar50@yahoo.com>
2019-05-23 15:27:56 +0530

    Add files via upload

--- Assignments/14_ascend_descend.sh
+++ Assignments/14_ascend_descend.sh
@@ -4,12 +4,67 @@
 #script to sort a given number in ascending or descending order.
 
 
-
+tmp=0
+arr=($*)
 p=$#
-q=$@
-t1=$1
-for i in $q
+k=$(expr $p - 1)
+
+echo select 1. Ascending 2. descending
+read z
+
+case $z in
+	
+	1) 	
+
+		for i in $(seq 0 $k)
+		do
+		j=0
+		l=$(expr $k - $i)
+	
+			while [ $j -lt $l ]
+			do
+			b=$(expr $j + 1) 
+				if [ ${arr[$j]} -gt ${arr[$b]} ]
+				then
+					tmp=${arr[$j]}	
+					arr[$j]=${arr[$b]}
+					arr[$b]=$tmp
+				fi
+				let j=$j+1
+			done
+		done
+		;;
+	2)
+
+		for i in $(seq 0 $k)
+		do
+		j=0
+		l=$(expr $k - $i)
+	
+			while [ $j -lt $l ]
+			do
+			b=$(expr $j + 1) 
+				if [ ${arr[$j]} -lt ${arr[$b]} ]
+				then
+					tmp=${arr[$j]}	
+					arr[$j]=${arr[$b]}
+					arr[$b]=$tmp
+				fi
+				let j=$j+1
+			done
+		done
+		;;
+	*)
+		;;
+esac
+	
+echo “The ordered array is……”
+for i in $(seq 0 ${#arr[*]})
 do
-	if [ $i -gt $t1 ]
-	then
-		arr[$n]=$
+	echo “${arr[$i]}”
+done
+
+
+
+
+
